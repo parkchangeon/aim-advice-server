2f52b279582e3a2fb90605cac8244ce08529d445
jg9870 <jg9870@naver.com>
2025-05-23 19:44:10 +0900

    test(user): 유저 role 변경 기능 테스트 코드 작성

--- src/test/java/com/aim/advice/controller/UserControllerTest.java
+++ src/test/java/com/aim/advice/controller/UserControllerTest.java
@@ -1,17 +1,24 @@
 package com.aim.advice.controller;
 
 import com.aim.advice.ControllerTestSupport;
+import com.aim.advice.domain.user.Role;
 import com.aim.advice.dto.user.SignupRequest;
+import com.aim.advice.dto.user.UpdateRoleRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@AutoConfigureMockMvc
 class UserControllerTest extends ControllerTestSupport {
 
     @DisplayName("신규 회원가입을 등록한다.")
@@ -140,6 +147,123 @@ class UserControllerTest extends ControllerTestSupport {
                 .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
                 .andExpect(jsonPath("$.message").value("password size must be between 8 and 16"))
                 .andExpect(jsonPath("$.data").doesNotExist());
+    }
+
+    @Test
+    @DisplayName("관리자가 사용자의 역할을 변경할 수 있다.")
+    @WithMockUser(username = "admin", roles = "ADMIN")
+    void updateRole() throws Exception {
+        // given
+        UpdateRoleRequest request = UpdateRoleRequest.of("user1", Role.ADMIN);
+
+        // when // then
+        mockMvc.perform(put("/api/v1/users/role")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value(200))
+                .andExpect(jsonPath("$.status").value("OK"))
+                .andExpect(jsonPath("$.message").value("OK"));
+    }
+
+    @Test
+    @DisplayName("관리자가 아닌 경우 사용자의 역할을 변경할 수 없다.")
+    @WithMockUser(username = "user")
+    void updateRoleWithNoAdmin() throws Exception {
+        // given
+        UpdateRoleRequest request = UpdateRoleRequest.of("user1", Role.ADMIN);
+
+        // when // then
+        mockMvc.perform(put("/api/v1/users/role")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andDo(print())
+                .andExpect(status().isForbidden())
+                .andExpect(jsonPath("$.code").value(403))
+                .andExpect(jsonPath("$.status").value("FORBIDDEN"))
+                .andExpect(jsonPath("$.message").value("Forbidden"));
+    }
+
+    @Test
+    @DisplayName("관리자가 사용자의 역할을 변경할 때 변경할 userId 값은 필수이다.")
+    @WithMockUser(username = "admin", roles = "ADMIN")
+    void updateRoleWithNoUserId() throws Exception {
+        // given
+        UpdateRoleRequest request = UpdateRoleRequest.of("", Role.ADMIN);
+
+        // when // then
+        mockMvc.perform(put("/api/v1/users/role")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value(400))
+                .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+                .andExpect(jsonPath("$.message").value("UserId is required"));
+    }
+
+    @Test
+    @DisplayName("관리자가 사용자의 역할을 변경할 때 변경할 role 값은 필수이다.")
+    @WithMockUser(username = "admin", roles = "ADMIN")
+    void updateRoleWithNoUserRole() throws Exception {
+        // given
+        UpdateRoleRequest request = UpdateRoleRequest.of("user1", null);
+
+        // when // then
+        mockMvc.perform(put("/api/v1/users/role")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value(400))
+                .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+                .andExpect(jsonPath("$.message").value("Role is required"));
+    }
+
+    @Test
+    @DisplayName("올바른 role 유형으로 role 변경 요청을 해야 한다.")
+    @WithMockUser(username = "admin", roles = "ADMIN")
+    void updateRoleWithRightRole() throws Exception {
+        // given
+        String invalidJson = """
+                    {
+                      "userId": "user1",
+                      "role": "INVALID"
+                    }
+                """;
+
+        // when // then
+        mockMvc.perform(put("/api/v1/users/role")
+                        .with(csrf())
+                        .content(invalidJson)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value(400))
+                .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+                .andExpect(jsonPath("$.message").value("The request body is missing or malformed"));
+    }
+
+    @Test
+    @DisplayName("role 변경할 때 request body가 아예 없는 경우 변경할 수 없다.")
+    @WithMockUser(roles = "ADMIN")
+    void updateRoleWithNoRequestBody() throws Exception {
+        // when // then
+        mockMvc.perform(
+                        put("/api/v1/users/role")
+                                .with(csrf())
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value(400))
+                .andExpect(jsonPath("$.status").value("BAD_REQUEST"))
+                .andExpect(jsonPath("$.message").value("The request body is missing or malformed"));
     }
 
 }
--- src/test/java/com/aim/advice/service/UserServiceTest.java
+++ src/test/java/com/aim/advice/service/UserServiceTest.java
@@ -1,11 +1,16 @@
 package com.aim.advice.service;
 
 import com.aim.IntegrationTestSupport;
+import com.aim.advice.domain.user.Role;
+import com.aim.advice.domain.user.User;
 import com.aim.advice.dto.user.SignupRequest;
 import com.aim.advice.dto.user.SignupResponse;
+import com.aim.advice.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -14,6 +19,9 @@ class UserServiceTest extends IntegrationTestSupport {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @DisplayName("아이디와 비밀번호를 받아 회원가입을 한다.")
     @Test
@@ -40,5 +48,28 @@ class UserServiceTest extends IntegrationTestSupport {
         assertThatThrownBy(() -> userService.signup(signupRequest))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("UserId already taken");
+    }
+
+    @DisplayName("회원 권한을 변경한다.")
+    @Test
+    void updateRole() {
+        // given
+        userRepository.save(User.of("user1", "password"));
+
+        // when
+        userService.updateRole("user1", Role.ADMIN);
+
+        // then
+        User user = userRepository.findByUserId("user1").orElseThrow();
+        assertThat(user.getRole()).isEqualTo(Role.ADMIN);
+    }
+
+    @DisplayName("회원 권한을 변경할 때 회원 아이디가 존재하지 않으면 예외가 발생한다.")
+    @Test
+    void updateRoleWithNoUser() {
+        // when // then
+        assertThatThrownBy(() -> userService.updateRole("user1", Role.ADMIN))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("User not found");
     }
 }
